be8f097690bda52d51adad815b5562e4074b1927
Muhammad Ibrahim <mibrahim37612@gmail.com>
2023-03-28 23:21:03 +0500

    [Add] Recipe Book

---
+++ Recipe_Form/script.js
@@ -0,0 +1,31 @@
+$(() => {
+  // These functions will be called when DOM is loaded
+  getData();
+});
+
+const getData = () => {
+  console.log("getData called");
+  //get data from the api
+  $.ajax({
+    url: "https://usman-fake-api.herokuapp.com/api/recipes",
+    method: "GET",
+    success: (response) => {
+      $("#table-loader").addClass("d-none");
+      for (let i = 0; i < response.length; i++) {
+        let row = `<tr>
+        <td><strong>${i + 1}</strong></td>
+            <td>${response[i].title}</td>
+            <td>${response[i].body}</td>
+            </tr>`;
+        $("#table-body").append(row);
+      }
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+const removeHide = () => {
+  $("#table-loader").addClass("d-none");
+};
