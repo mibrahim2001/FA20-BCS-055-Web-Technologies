d181cbb90903b86cb50b4940eb7c51eaf91dcc6a
Muhammad Ibrahim <mibrahim37612@gmail.com>
2023-05-28 22:23:09 +0500

    [Update] Improve auth.js

--- Final_Project/middleware/auth.js
+++ Final_Project/middleware/auth.js
@@ -1,4 +1,8 @@
-function auth(req, res) {
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../models/user");
+
+async function auth(req, res) {
   const token = req.header("x-auth-token");
   if (!token) {
     return res.status(401).send("Access denied. No token provided");
@@ -6,7 +10,7 @@ function auth(req, res) {
 
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-    req.user = decoded;
+    req.user = await User.findById(decoded._id);
     next();
   } catch (e) {
     res.status(400).send("Invalid token");
