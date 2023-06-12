495e4d8b84d5120f9d207fd336622d3cbd58191d
Muhammad Ibrahim <mibrahim37612@gmail.com>
2023-06-12 16:18:15 +0500

    [Update] Remove Final Project from this folder

--- Assignment_4/Restful/Backend/models/voucher.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-let modelSchema = mongoose.Schema({
-  title: String,
-  amount: Number,
-  isFixed: Boolean,
-  maxAmount: Number,
-});
-let Voucher = mongoose.model("Voucher", modelSchema);
-module.exports = Voucher;
--- Final_Project/middleware/checkSessionAuth.js
+++
@@ -1,8 +0,0 @@
-async function checkSessionAuth(req, res, next) {
-  if (!req.session.user) {
-    return res.redirect("/login");
-  }
-  next();
-}
-
-module.exports = checkSessionAuth;
